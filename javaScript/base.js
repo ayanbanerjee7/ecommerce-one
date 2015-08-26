2abcba848dc0e3bcdc968d56b9f51040087de68d
Ayan Banerjee <ayanbanerjeecse2008.gmail@gmail.com>
2015-08-26 12:47:12 -0400

    added angular js projects

--- javaScript/base.js
+++ javaScript/base.js
@@ -1,1 +1,7 @@
-alert("test");
+(function(){
+  var gem = { name: 'Azurite', price: 2.95 };
+  var app = angular.module('gemStore', []);
+  app.controller('StoreController',function(){
+    this.product=gem;
+  });
+})();
